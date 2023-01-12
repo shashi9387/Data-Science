253bf170b192fd3bdb70d7b700c65112f50a9851
Shashi Kumar <90020113+shashi9387@users.noreply.github.com>
2023-01-12 13:30:58 +0530

    Add files via upload

---
+++ 100 Days Challenge code/Day 1/main.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+print("Program To Guess Your Band Name \n")
+cityName = input("Type your city name: ")
+favouratePet = input("Type your pet name: ")
+print("Your suggested band name be:", cityName, favouratePet)
